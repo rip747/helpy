3fe0125900ccc2c6cd4187d5ba26ede2687208fc
Anthony Petruzzi <karalootzkoding@gmail.com>
2025-03-19 20:03:01 +0000

    upgrading

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,8 @@ module Api
     end
     #config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
